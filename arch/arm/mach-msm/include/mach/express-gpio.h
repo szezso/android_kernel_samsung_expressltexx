060e44f5fa6cefd790e7e47fe5d32d298d668608
szezso <szezsoo@gmail.com>
2014-08-14 15:52:27 +0200

    arch: arm: Add support for GT-I8730 (Galaxy Express)

--- arch/arm/mach-msm/include/mach/express-gpio.h
+++ arch/arm/mach-msm/include/mach/express-gpio.h
@@ -20,115 +20,117 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-/* MSM8960 GPIO */
-#define GPIO_MDP_VSYNC			0
-#define GPIO_MHL_RST			1
-#define GPIO_MSM_FLASH_CNTL_EN	2
-#define GPIO_CAM_MCLK2			2	/* >= REV03 */
+/* MSM8930 GPIO */
+#define GPIO_MSM_FLASH_CNTL_EN	-1	/* 2 is MHL_EN */
 #define GPIO_MSM_FLASH_NOW		3
-#define GPIO_MAIN_CAM_STBY		4
-#define GPIO_CAM_MCLK				5
-#define GPIO_CAM_CORE_EN			6
-#define GPIO_CODEC_I2C_SDA              8
-#define GPIO_CODEC_I2C_SCL              9
-#define GPIO_LCD_22V_EN			10
-#define GPIO_MXT_TS_IRQ			11
-#define GPIO_ALS_SDA			12
-#define GPIO_ALS_SCL			13
-#define GPIO_FPGA_CS			14
-#define GPIO_MHL_WAKE_UP		15
-#define GPIO_VT_STBY				18
+#define GPIO_SUB_CAM_MCLK		4
+#define GPIO_MAIN_CAM_MCLK		5
+#define GPIO_CAM_CORE_EN		6
 
-#define GPIO_MHL_EN				19
-#define GPIO_I2C_DATA_CAM				20
-#define GPIO_I2C_CLK_CAM				21
-#define GPIO_FUELGAUGE_I2C_SDA		24
-#define GPIO_FUELGAUGE_I2C_SCL		25
+#define GPIO_NC_4		4
+#define GPIO_NC_51		51
+#define GPIO_NC_47		47
+#define GPIO_NC_52		52
 
-#define GPIO_NFC_SDA			32
-#define GPIO_NFC_SCL			33
-#ifdef CONFIG_VP_A2220
-#define GPIO_A2220_I2C_SDA		36
-#define GPIO_A2220_I2C_SCL		37
-#endif
-#define GPIO_KEY_HOME			40
+#define GPIO_TOUCH_IRQ		11
+#define GPIO_TOUCH_SDA		16
+#define GPIO_TOUCH_SCL		17
 
-#define GPIO_ALS_INT			-1 /*42*/
-#define GPIO_SENSOR_SNS_SDA		44
-#define GPIO_SENSOR_SNS_SCL		45
-#define GPIO_CAM_A_EN			46
-#define GPIO_HAPTIC_PWR_EN              47
+#define GPIO_VT_STBY			18
+#define GPIO_I2C_DATA_CAM		20
+#define GPIO_I2C_CLK_CAM		21
+#define GPIO_UART_RXD		23
 
-#define GPIO_VOLUME_UP			50
-#define GPIO_MXT_TS_LDO_EN		-1
+#define GPIO_CAM_A_EN			38
+#define GPIO_SD_CARD_DET_N	39
+#define GPIO_CAM_IO_EN		34  /* 34 in rev01 */
+#define GPIO_HOME_KEY		35
+#define GPIO_VOLUME_UP		50
 #define GPIO_TKEY_LED			51
-#define GPIO_MXT_TS_RESET		-1
-#ifdef CONFIG_KEYBOARD_CYPRESS_TOUCH
-#define	GPIO_TOUCH_KEY_INT_02	52
-#define	GPIO_TOUCH_KEY_INT		54
-#endif
 
-#define GPIO_SENSOR_ALS_SDA		63
-#define GPIO_SENSOR_ALS_SCL		64
-#define GPIO_MAG_RST			66
-#define GPIO_FUEL_INT			67
-#ifdef CONFIG_MPU_SENSORS_MPU6050B1_411
-#define GPIO_MSENSE_RST                 68
-#define GPIO_MPU3050_INT                69
-#endif
-#define GPIO_VIB_PWM                    70
+#define GPIO_MAIN_STBY			54
+#define GPIO_VIB_EN			63
+#define GPIO_FLASH_LED_UNLOCK   64
+#define GPIO_TKEY_INT			65
+#define GPIO_CAM_AF_EN			66
+#define GPIO_VIB_PWM			70
+#define GPIO_TKEY_I2C_SDA		71
+#define GPIO_TKEY_I2C_SCL		72
+#define GPIO_TKEY_I2C_SDA_REV02		24
+#define GPIO_TKEY_I2C_SCL_REV02		25
+#define GPIO_USB_I2C_SDA                73
+#define GPIO_USB_I2C_SCL                74
+#define GPIO_CAM2_RST_N			76
+#define GPIO_TSP_D_EN			79
+#define GPIO_TSP_A_EN			80
+#define GPIO_VOLUME_DOWN	81
+#define GPIO_TKEY_LDO_EN		99
+#define GPIO_CAM1_RST_N			107
 
-#ifdef CONFIG_KEYBOARD_CYPRESS_TOUCH
-#define GPIO_TOUCHKEY_SDA		71
-#define GPIO_TOUCHKEY_SCL		72
-#endif
-#define GPIO_USB_I2C_SDA		73
-#define GPIO_USB_I2C_SCL		74
-#define GPIO_CAM2_RST_N				76
-#define GPIO_MHL_SEL			82
+/* BATTERY */
+#define GPIO_BATT_INT	7
 
-#define GPIO_VOLUME_DOWN		81
-#define GPIO_KS8851_RST			89
-#define GPIO_KS8851_IRQ			90
-
-#define GPIO_NFC_FIRMWARE		92
-#ifdef CONFIG_KEYBOARD_CYPRESS_TOUCH
-#define	GPIO_TOUCH_KEY_EN		94
-#endif
-#define GPIO_MHL_SDA			95
-#define GPIO_MHL_SCL			96
-#define GPIO_MHL_INT			99
-
+/* SENSORS */
+#define GPIO_NFC_SDA			95
+#define GPIO_NFC_SCL			96
 #define GPIO_NFC_IRQ			106
-#define GPIO_CAM1_RST_N				107
+#define GPIO_NFC_FIRMWARE		92
+#define GPIO_NFC_EN			48
+#define GPIO_NFC_CLK_REQ		90
 
-/* MSM8960 NC GPIOs */
-#define GPIO_NC_7				7
-#define GPIO_NC_42				42
-#define GPIO_NC_68				68
+#define GPIO_SENSOR_ALS_SDA		12
+#define GPIO_SENSOR_ALS_SCL		13
+#define GPIO_ACC_INT_N			67
+#define GPIO_GYRO_INT			67
+#define GPIO_LEDA_EN			89
+#define GPIO_PROX_INT			49
 
-/* ES305B GPIO */
-#define MSM_AUD_A2220_WAKEUP		35
-#define MSM_AUD_A2220_RESET		75
+/* MHL/HDMI */
+#define GPIO_MHL_RST			1
+#define GPIO_MHL_EN				2
+#define GPIO_MHL_SDA			8
+#define GPIO_MHL_SCL			9
+#define GPIO_MHL_WAKE_UP		77
+#define GPIO_MHL_INT			78 /* 55 for Rev00 */
+#define GPIO_MHL_SEL			82
+#define GPIO_VPS_SEL			52
 
-/* PMIC8921 MPP */
-#define PMIC_MPP_FLASH_LED_UNLOCK       4
+/* AUDIO */
+#define GPIO_LEFT_SPK			-1	/* 1 -> MHL_RST */
+#define GPIO_CODEC_INT			10
+#define GPIO_CODEC_RESET		42
+#define GPIO_CODEC_SDA			36
+#define GPIO_CODEC_SCK			37
 
-/* PMIC8921 GPIO */
-#define PMIC_GPIO_CHG_EN		2
-#define PMIC_GPIO_VIB_ON		4
-#define PMIC_GPIO_LINEOUT_EN		5
-#define PMIC_MSM_FLASH_CNTL_EN		5
-#define PMIC_GPIO_ALS_INT               6
-#define PMIC_GPIO_CHG_STAT		17
-#define PMIC_GPIO_SPK_EN		18
-#define PMIC_GPIO_VPS_EN		19 /* NC */
-#define PMIC_GPIO_NFC_EN		21
-#define PMIC_GPIO_OTG_EN		22
-#define PMIC_GPIO_BATT_INT		37
-#define PMIC_GPIO_CODEC_RST		38
-#define PMIC_GPIO_OTG_POWER		42
-#define PMIC_GPIO_LCD_RST		43
+#define GPIO_SPKR_I2S_TX_SCK	55
+#define GPIO_SPKR_I2S_TX_WS		56
+#define GPIO_SPKR_I2S_TX_DIN	57
+#define GPIO_AUDIO_MCLK_REV10	59
+#define GPIO_AUDIO_MCLK			53
+#define GPIO_SPKR_I2S_RX_SCK	60
+#define GPIO_SPKR_I2S_RX_DOUT	61
+#define GPIO_SPKR_I2S_RX_WS		62
+
+#define GPIO_VPS_AMP_EN		0
+#define GPIO_SHORT_SENDEND		94
+#if defined(CONFIG_SAMSUNG_JACK_GNDLDET)
+#define GPIO_EAR_DET			43
+#define GPIO_EAR_GND			46
+#else
+#define GPIO_EAR_DET			46
+#define GPIO_EAR_GND			43
+#endif
+#define GPIO_EAR_MIC_BIAS_EN		151
+#define GPIO_SPK_AMP_EN		PM8038_GPIO_PM_TO_SYS(14)
+
+
+/* OTG */
+#define GPIO_OTG_TEST		150
+#define GPIO_OVP_CTRL		PM8038_GPIO_PM_TO_SYS(15)
+
+#if defined(CONFIG_WCD9304_CLK_9600)
+#define CLK_REVISION 8
+#endif
 
 /* gpio for changed list */
 enum {
@@ -149,9 +151,4 @@ enum {
 	BOARD_REV14,
 	BOARD_REV15,
 	GPIO_REV_MAX,
-};
-
-enum {
-	MDP_VSYNC,
-	PS_INT,
 };
