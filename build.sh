ea1b6d74d79fad8da6dfd5200f6b36fee2558f8d
szezso <szezsoo@gmail.com>
2014-08-14 15:18:30 +0200

    Initial upload from SM-G3815_EUR_KK_Opensource.zip

---
+++ build.sh
@@ -0,0 +1,3 @@
+make ARCH=arm CROSS_COMPILE=/home/gaurav.k6/wilkox_build/android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-  VARIANT_DEFCONFIG=msm8930_cane_wilcox_eur_lte_defconfig  SELINUX_DEFCONFIG=selinux_defconfig msm8930_cane_defconfig
+make -j 32 ARCH=arm CROSS_COMPILE=/home/gaurav.k6/wilkox_build/android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+
